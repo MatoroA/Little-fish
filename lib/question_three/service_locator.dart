b62a824673b2663b85a173834039cbbce9ef6b9a
MatoroA <andanimatoro@gmail.com>
2023-09-06 20:01:46 +0200

    feat: register repository, data source

--- lib/question_three/service_locator.dart
+++ lib/question_three/service_locator.dart
@@ -3,13 +3,16 @@ import 'package:littlefish/question_three/api/data_sources/character_api_data_so
 import 'package:littlefish/question_three/api/data_sources/character_api_data_source_impl.dart';
 import 'package:littlefish/question_three/core/global_configuration.dart';
 import 'package:http/http.dart' as http;
+import 'package:littlefish/question_three/repositories/character_repository.dart';
+import 'package:littlefish/question_three/repositories/character_repository_impl.dart';
 
 final serviceLocator = GetIt.instance;
 final _globalConfiguration = GlobalConfiguration();
 final _httpClient = http.Client();
 
-void registerServices() async {
+Future<void> registerServices() async {
   await _loadGlobalConfiguration();
+  _registerDatasource();
   _registerRepository();
 }
 
@@ -27,11 +30,19 @@ String get baseUrl {
   return config.tryGetValue('baseUrl', '');
 }
 
-void _registerRepository() {
+void _registerDatasource() {
   serviceLocator.registerLazySingleton<CharacterApiDataSource>(
     () => CharacterApiDataSourceImpl(
       client: _httpClient,
       baseUrl: baseUrl,
+    ),
+  );
+}
+
+void _registerRepository() {
+  serviceLocator.registerLazySingleton<CharacterRepository>(
+    () => CharacterRepositoryImpl(
+      dataSource: serviceLocator.get<CharacterApiDataSource>(),
     ),
   );
 }
