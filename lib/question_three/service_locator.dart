3211d6ca5497679c1675c8bf9307de963521a0f1
MatoroA <andanimatoro@gmail.com>
2023-09-08 06:00:03 +0200

    feat: refactor

--- lib/question_three/service_locator.dart
+++ lib/question_three/service_locator.dart
@@ -1,10 +1,13 @@
 import 'package:get_it/get_it.dart';
-import 'package:littlefish/question_three/api/data_sources/character_api_data_source.dart';
-import 'package:littlefish/question_three/api/data_sources/character_api_data_source_impl.dart';
+import 'package:littlefish/question_three/data/data_sources/character_api_data_source.dart';
+import 'package:littlefish/question_three/data/data_sources/character_api_data_source_impl.dart';
 import 'package:littlefish/question_three/core/global_configuration.dart';
 import 'package:http/http.dart' as http;
-import 'package:littlefish/question_three/repositories/character_repository.dart';
-import 'package:littlefish/question_three/repositories/character_repository_impl.dart';
+import 'package:littlefish/question_three/data/database/dao/characters_dao.dart';
+import 'package:littlefish/question_three/data/database/mapper/database_mapper.dart';
+import 'package:littlefish/question_three/data/database/mapper/database_mapper_impl.dart';
+import 'package:littlefish/question_three/domain/repositories/character_repository.dart';
+import 'package:littlefish/question_three/domain/repositories/character_repository_impl.dart';
 
 final serviceLocator = GetIt.instance;
 final _globalConfiguration = GlobalConfiguration();
@@ -14,6 +17,12 @@ Future<void> registerServices() async {
   await _loadGlobalConfiguration();
   _registerDatasource();
   _registerRepository();
+  _registerDbDao();
+  _registerMapper();
+}
+
+void _registerDbDao() {
+  serviceLocator.registerLazySingleton(() => CharactersDao());
 }
 
 Future<void> _loadGlobalConfiguration() async {
@@ -43,6 +52,14 @@ void _registerRepository() {
   serviceLocator.registerLazySingleton<CharacterRepository>(
     () => CharacterRepositoryImpl(
       dataSource: serviceLocator.get<CharacterApiDataSource>(),
+      charactersDao: serviceLocator.get<CharactersDao>(),
+      databaseMapper: serviceLocator.get<DatabaseMapper>(),
     ),
+  );
+}
+
+void _registerMapper() {
+  serviceLocator.registerLazySingleton<DatabaseMapper>(
+    () => DatabaseMapperImpl(),
   );
 }
